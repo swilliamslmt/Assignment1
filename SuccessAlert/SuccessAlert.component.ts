74735326459dc31ff241bfa44f873785b02b560a
Stephanie <40765785+swilliamslmt@users.noreply.github.com>
2019-02-10 23:39:35 -0600

    Add files via upload

---
+++ SuccessAlert/SuccessAlert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+selector: 'app-successalert',
+templateUrl: './successalert.component.html',
+styleUrls: ['./successalert.component.css']
+})
+export class SuccessAlertComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() {
+    }
+}
+
